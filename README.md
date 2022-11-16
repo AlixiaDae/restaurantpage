668886a614355cc67ee5d65d8895342e751a31b7
AlixiaDae <lizziejones1208@gmail.com>
2022-11-17 04:24:22 +0800

    Add main content with restaurant motto

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 Photo by <a href="https://unsplash.com/@louishansel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Louis Hansel</a> on <a href="https://unsplash.com/s/photos/salad?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   
+
+Crab Salad by Des Gomez on <a href="https://www.dafont.com/crabsalad.font">dafont</a>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import '../styles/home.css'
+import Salad from '../images/salad.jpg'
 
 function headerBar() {
     const headerContainer = document.createElement('div')
@@ -47,25 +48,57 @@ function headerContent() {
     const headerContainer = document.querySelector('.header-container')
     const messageWrapper = document.createElement('div')
     const mainMessage = document.createElement('div')
+    const searchWrapper = document.createElement('div')
     const searchBar = document.createElement('input')
+    const searchBtn = document.createElement('button')
     const imageCredit = document.createElement('a')
     const imageText = document.createTextNode('Photo by Louis Hansel on Unsplash')
 
-    messageWrapper.classList.add('main-message-wrapper')
-    mainMessage.classList.add('main-message')
+    messageWrapper.classList.add('message-wrapper')
+    mainMessage.classList.add('message')
+    searchWrapper.classList.add('search-wrapper')
     searchBar.classList.add('searchbar')
+    searchBtn.classList.add('search-button')
     imageCredit.classList.add('main-image-credit')
 
     mainMessage.textContent = "Experience the freshness of life!"
     searchBar.placeholder = "Search for a store near you!"
+    searchBtn.textContent = "Search"
     imageCredit.title = "my text"
     imageCredit.href = "https://unsplash.com/s/photos/salad?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
 
     messageWrapper.appendChild(mainMessage)
-    messageWrapper.appendChild(searchBar)
+    searchWrapper.appendChild(searchBar)
+    searchWrapper.appendChild(searchBtn)
+    messageWrapper.appendChild(searchWrapper)
     imageCredit.appendChild(imageText)
     messageWrapper.appendChild(imageCredit)
     return headerContainer.appendChild(messageWrapper)
+}
+
+function content() {
+    const contentContainer = document.createElement('div')
+    const subHeadWrapper = document.createElement('div')
+    const subHeader = document.createElement('h2')
+    const sub = document.createElement('div')
+    const salad = document.createElement('img')
+
+    subHeader.textContent = "Looking to refresh yourself?"
+    sub.textContent = `Here at Salad Bowl it is our belief that fresh and healthy are the best ingredients to a green and peaceful lifestyle! Sourced from local ingredients, our bowls are guaranteed to reflect nature around us, which means, other than our regular menu, each location may have their very own specialties.`;
+
+    contentContainer.classList.add('content-container')
+    subHeadWrapper.classList.add('sub-wrapper')
+    subHeader.classList.add('sub-header')
+    sub.classList.add('sub')
+    salad.classList.add('salad')
+
+    salad.src = Salad
+    subHeadWrapper.appendChild(subHeader)
+    subHeadWrapper.appendChild(sub)
+    contentContainer.appendChild(subHeadWrapper)
+    contentContainer.appendChild(salad)
+
+    return document.body.appendChild(contentContainer)
 }
 
 
@@ -73,6 +106,7 @@ function headerContent() {
 function render() {
     headerBar()
     headerContent()
+    content()
 }
 
 export default render;
